e3ae37d1897df08857b20c6f9275fd6d867ee68c
David Valdman <dmvaldman@gmail.com>
2020-06-21 22:59:32 -0700

    Updates

--- static/js/app.js
+++ static/js/app.js
@@ -5,67 +5,87 @@ const searchResultsEl = document.getElementById("searchResults");
 url_base = "http://localhost:8000/"
 // url_base = "https://affine-news.appspot.com/"
 
-var countriesPath = 'static/data/countries.geo.json';
-
-function getColor(data) {
-    let d = 0
-    if (data instanceof Array)  d = data.length
-    return d > 7 ? '#800026':
-        d > 20 ? '#BD0026':
-        d > 10 ? '#E31A1C':
-        d > 5 ? '#FC4E2A':
-        d > 2 ? '#FD8D3C':
-        d > 1 ? '#FEB24C':
-        d > 0 ? '#FED976':
-                '#FFEDA0';
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.affine),
-        weight: 1,
-        opacity: 1,
-        color: 'white',
-        dashArray: '1',
-        fillOpacity: 0.7
-    };
-}
-
-
-let geojsonLayer = null;
-let geojsonData = null;
-let map = null;
-
-fetch(url_base + countriesPath)
-    .then(response => response.json())
-    .then(data => {
-        map = L.map('map').setView([45, 0], 1);
-        geojsonData = data
-        geojsonData = updateData(data, {})
-        geojsonLayer = L.geoJson(geojsonData, {
-            clickable: false,
-            style: style
-        }).addTo(map);
+let map = new Datamap({
+    element: document.getElementById('map'),
+    projection: 'mercator',
+    fills: {
+        defaultFill: 'rgba(182,184,196,0.9)' // Any hex, color name or rgb/rgba value
+    },
+    geographyConfig: {
+        highlightOnHover: false
+    },
+    done: function(datamap) {
+        datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+            let el = document.getElementById(geography.properties.name)
+            if (el) el.click()
+        })
+    }
 });
 
 $(function() {
     $('input[name="dates"]').daterangepicker({
-        opens: 'left'
+        opens: 'left',
+        ranges: {
+            'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
     }, function(start, end, label) {
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
 });
 
-function updateData(data, patch){
-    for (let index in geojsonData.features){
-        let countryData = geojsonData.features[index].properties
-        countryData['affine'] = patch[countryData.name]
-    }
-    return data
+const countryToISO = {
+    'USA': 'USA',
+    'Sweden': 'SWE',
+    'Italy': 'ITA',
+    'France': 'FRA',
+    'Spain': 'ESP',
+    'China': 'CHN',
+    'Poland': 'POL',
+    'Ukraine': 'UKR',
+    'Syria': 'SYR',
+    'Russia': 'RUS',
+    'Iraq': 'IRQ',
+    'Iran': 'IRN',
+    'Turkey': 'TUR',
+    'Qatar': 'QAT',
+    'Hong Kong': 'HKG',
+    'Brazil': 'BRA'
 }
 
+function formatData(data){
+    let formattedData = {}
+    let lengthMin = Infinity
+    let lengthMax = -Infinity
 
+    for (let country in data){
+        let iso = countryToISO[country]
+        formattedData[iso] = data[country]
 
+        let length = data[country].length
+        if (length > lengthMin) lengthMin = length
+        if (length < lengthMax) lengthMax = length
+    }
+
+    let paletteScale = d3.scale.linear()
+            .domain([lengthMin,lengthMax])
+            .range([
+                "#EFEFFF",
+                "#02386F"]
+            );
+
+    for (let country in data){
+        let iso = countryToISO[country]
+        formattedData[iso] = Object.assign({}, data[country])
+        formattedData[iso]['fillColor'] = paletteScale(data[country].length)
+    }
+
+    return formattedData
+}
 
 searchQueryEl.onkeypress = function(e){
     let code = (e.keyCode ? e.keyCode : e.which);
@@ -103,26 +123,39 @@ function search(){
         })
         .then(function(data){
 
-            map.removeLayer(geojsonLayer)
-            geojsonData = updateData(geojsonData, data)
-            geojsonLayer = L.geoJson(geojsonData, {
-                clickable: false,
-                style: style
-            }).addTo(map);
+            map.updateChoropleth(formatData(data), {reset: true})
 
             searchResultsEl.innerHTML = ''
 
             for (let country in data){
                 let countryEl = document.createElement('ul')
-                countryEl.textContent = country
+                let anchorEl = document.createElement('a')
+                anchorEl.textContent = country + ' (' + data[country].length + ')'
+                anchorEl.href = '#' + country
+                anchorEl.name = country
+                anchorEl.id = country
+                countryEl.appendChild(anchorEl)
 
                 for (let result in data[country]){
+                    let date = new Date(data[country][result].publish_at).toDateString()
+                    let url = data[country][result].article_url
+
                     let resultEl = document.createElement('li')
+
+                    let dateEl = document.createElement('div')
+                    let dateTextEl = document.createTextNode(date)
+                    dateEl.classList.add('date')
+                    dateEl.appendChild(dateTextEl)
+
                     let urlEl = document.createElement('a')
                     let textEl = document.createTextNode(data[country][result].title)
                     urlEl.appendChild(textEl)
-                    urlEl.href = 'https://translate.google.com/translate?hl=&sl=auto&tl=en&u=' + data[country][result].article_url
+
+                    urlEl.href = 'https://translate.google.com/translate?hl=&sl=auto&tl=en&u=' + url
+
+                    resultEl.appendChild(dateEl)
                     resultEl.appendChild(urlEl)
+
                     countryEl.appendChild(resultEl)
                 }
 
