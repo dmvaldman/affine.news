4752106f4095b9f3b9b656403856c894c294311d
David Valdman <dmvaldman@gmail.com>
2020-09-07 23:09:53 -0700

    Add 3day date range version

--- static/js/app.js
+++ static/js/app.js
@@ -35,6 +35,7 @@ $(function() {
         ranges: {
             'Today': [moment(), moment()],
             'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 3 Days': [moment().subtract(2, 'days'), moment()],
             'Last 7 Days': [moment().subtract(6, 'days'), moment()],
             'Last 30 Days': [moment().subtract(29, 'days'), moment()]
         }
