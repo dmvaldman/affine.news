f74df196c2affe665b1ae4e58055f441ed3891b7
David Valdman <dmvaldman@gmail.com>
2020-12-30 21:29:33 -0800

    Better json response

--- server/services/stats.py
+++ server/services/stats.py
@@ -1,3 +1,5 @@
+import datetime
+
 from server.db.db import conn
 from psycopg2.extras import DictCursor
 
@@ -8,10 +10,13 @@ def occurences_over_time(query, date_start, date_end, country=None):
           date,
           iso,
           total,
-          avg(total) OVER (
-            PARTITION BY iso
-            ORDER BY date ROWS BETWEEN 3 PRECEDING AND 3 FOLLOWING
-          ) as rolling
+          CAST(
+            avg(total) 
+                OVER (
+                    PARTITION BY iso
+                    ORDER BY date ROWS BETWEEN 3 PRECEDING AND 3 FOLLOWING
+                ) 
+            as FLOAT) as rolling
         FROM (
           SELECT 
             DATE(a.publish_at) as date,
@@ -33,4 +38,31 @@ def occurences_over_time(query, date_start, date_end, country=None):
         c.execute(command, params)
         results = c.fetchall()
 
-    return results
+    obj = {}  # type sig: {iso: {date, iso, total, rolling}}
+    for result in results:
+        iso = result['iso']
+
+        if iso not in obj:
+            obj[iso] = []
+
+        datum = {}
+        for key in result.keys():
+            datum[key] = result[key]
+
+        obj[iso].append(datum)
+    return obj
+
+
+if __name__ == '__main__':
+    query_str = 'Trump'
+    today = datetime.date.today()
+    date_start = today - datetime.timedelta(days=7)
+    date_end = today - datetime.timedelta(days=0)
+
+    params = {
+        'query': query_str,
+        'date_start': str(date_start),
+        'date_end': str(date_end)
+    }
+    query_result = occurences_over_time(query_str, date_start, date_end)
+    print(query_result)
