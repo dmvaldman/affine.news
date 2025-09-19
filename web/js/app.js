03e843814b74bc699d4486eec11b906010042b74
David Valdman <dmvaldman@gmail.com>
2025-09-18 21:57:23 -0700

    SVGGGGGs

--- web/js/app.js
+++ web/js/app.js
@@ -206,7 +206,7 @@ async function search(){
 
     const separator = document.querySelector('.section-separator');
     if (separator) {
-        separator.textContent = "Loading...";
+        separator.textContent = "Loading";
         separator.classList.add('visible');
     }
 
@@ -354,7 +354,11 @@ async function search(){
     }
 
     searchButtonEl.disabled = false;
-    searchButtonEl.innerHTML = 'Search';
+    searchButtonEl.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+            </svg>
+        `;
 }
 
 function applySummaryToMap(summary){
